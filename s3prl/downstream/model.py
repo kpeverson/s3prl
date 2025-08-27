e16b642516c36a7c914bd16dcddc0a83bd99fdb8
KEVIN EVERSON <kpever@g3071.hyak.local>
2025-08-27 11:13:53 -0700

    add before/after pooling

--- s3prl/downstream/model.py
+++ s3prl/downstream/model.py
@@ -57,6 +57,40 @@ class UtteranceLevel(nn.Module):
 
         return logit, features_len
 
+class UtteranceLevelBeforeAfter(nn.Module):
+    def __init__(self,
+        input_dim,
+        output_dim,
+        pooling='MeanPooling',
+        activation='ReLU',
+        pre_net=None,
+        post_net={'select': 'FrameLevel'},
+        **kwargs
+    ):
+        super().__init__()
+        latest_dim = input_dim
+        self.pre_net_before = get_downstream_model(latest_dim, latest_dim, pre_net) if isinstance(pre_net, dict) else None
+        self.pre_net_after = get_downstream_model(latest_dim, latest_dim, pre_net) if isinstance(pre_net, dict) else None
+        self.pooling_before = eval(pooling)(input_dim=latest_dim, activation=activation)
+        self.pooling_after = eval(pooling)(input_dim=latest_dim, activation=activation)
+        self.post_net = get_downstream_model(latest_dim*2, output_dim, post_net)
+
+    def forward(self, hidden_state, features_len):
+        assert isinstance(hidden_state, (list, tuple)) and len(hidden_state) == 2, \
+            f"hidden_state should be a list or tuple of length 2, got {type(hidden_state)} with length {len(hidden_state)}"
+        assert isinstance(features_len, (list, tuple)) and len(features_len) == 2, \
+            f"features_len should be a list or tuple of length 2, got {type(features_len)} with length {len(features_len)}"
+        if self.pre_net_before is not None and self.pre_net_after is not None:
+            hidden_state[0], features_len[0] = self.pre_net_before(hidden_state[0], features_len[0])
+            hidden_state[1], features_len[1] = self.pre_net_after(hidden_state[1], features_len[1])
+
+        pooled_before, features_len[0] = self.pooling_before(hidden_state[0], features_len[0])
+        pooled_after, features_len[1] = self.pooling_after(hidden_state[1], features_len[1])
+        pooled = torch.cat([pooled_before, pooled_after], dim=-1)
+        features_len = [f1+f2 for f1, f2 in zip(features_len[0], features_len[1])]
+        logit, features_len = self.post_net(pooled, features_len)
+
+        return logit, features_len
 
 class MeanPooling(nn.Module):
 
